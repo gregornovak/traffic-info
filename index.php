67281a0f17786c016050b14a296b1533466a5d41
gregornovak <grega_novak@yahoo.com>
2017-04-03 22:30:37 +0200

    Added input fields

--- index.php
+++ index.php
@@ -1,33 +1,42 @@
 <?php
 
-class Conditions {
+class Traffic {
+
+    public $location = '';
 
     public function getConditions() {
-        // Get cURL resource
-        $curl = curl_init();
-        // Set some options - we are passing in a useragent too here
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'http://www.promet.si/dc/PROMET.ROADEVENTS.PP.RSS.SL'
-        ));
-        // Send the request & save response to $resp
-        $resp = curl_exec($curl);
-        if(!curl_exec($curl)){
-            die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
-        }
-        // Close request to clear up some resources
-        curl_close($curl);
-        $xml = new SimpleXMLElement($resp);
-        $events = [];
-        $reg = "/A1/";
+        if(isset($_POST)) {
+            // Get cURL resource
+            $this->location = $_POST['location'];
 
-        foreach($xml->entry as $e){
-            if(preg_match($reg, $e->title)){
-                $events[] = $e;
+            $curl = curl_init();
+            // Set some options - we are passing in a useragent too here
+            curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'http://www.promet.si/dc/PROMET.ROADEVENTS.PP.RSS.SL'
+            ));
+            // Send the request & save response to $resp
+            $resp = curl_exec($curl);
+            if(!curl_exec($curl)){
+                die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
             }
+            // Close request to clear up some resources
+            curl_close($curl);
+            $xml = new SimpleXMLElement($resp);
+            $events = [];
+            $reg = "/$this->location/i";
+
+            foreach($xml->entry as $e){
+                if(preg_match($reg, $e->title)){
+                    $events[] = $e;
+                }
+            }
+
+            return $events;
+        } else {
+            return 'Vpišite željeno lokacijo';
         }
 
-        return $events;
     }
 
 }
--- index.view.php
+++ index.view.php
@@ -5,21 +5,46 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="lib/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700" rel="stylesheet">
     <title>Razmere na cestah</title>
 </head>
 <body>
-<div>
-    <h1>Razmere:</h1>
-    <ul>
-        <?php
-        $conditions = new Conditions();
-        foreach($conditions->getConditions() as $c):
-        ?>
-            <li><?php echo $c->title . ", " . $c->updated; ?></li>
-        <?php
-        endforeach;
-        ?>
-    </ul>
-</div>
+    <div id="wrapper">
+        <main>
+            <div class="traffic-heading">
+                <h1>Prometni pomočnik</h1>
+            </div>
+            <div class="user-input-wrapper">
+                <form action="index.php" method="POST">
+                    <div class="input-wrapper">
+                        <label for="location">Lokacija: </label>
+                        <input type="text" id="location" name="location">
+                    </div>
+                    <button type="submit">Pridobi Info</button>
+                </form>
+<!--                <div class="input-wrapper">-->
+<!--                    <label for="from">Od: </label>-->
+<!--                    <input type="text" id="from" name="from" class="user-input">-->
+<!--                </div>-->
+<!--                <div class="input-wrapper">-->
+<!--                    <label for="from">Do: </label>-->
+<!--                    <input type="text" id="from" name="from" class="user-input">-->
+<!--                </div>-->
+            </div>
+            <ul>
+                <?php
+                $conditions = new Traffic();
+                foreach($conditions->getConditions() as $c):
+                    ?>
+                    <li>
+                        <?php echo $c->category['term'] . ' ' . $c->title . ": " . $c->content . ", Osveženo: " . date('H:i - j, M Y', strtotime($c->updated)); ?>
+                    </li>
+                    <?php
+                endforeach;
+                ?>
+            </ul>
+        </main>
+    </div>
 </body>
 </html>
